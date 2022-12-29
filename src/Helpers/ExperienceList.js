446a7614846835c14bd5ae847ac13e407e01864e
kevinshark <chile109@gmail.com>
2022-12-29 14:39:12 +0800

    add: experience list helper and map it in page

---
+++ src/Helpers/ExperienceList.js
@@ -0,0 +1,76 @@
+import WorkIcon from "@material-ui/icons/Work";
+// import SchoolIcon from "@material-ui/icons/School";
+
+export const ExperienceList = [
+    {
+        Format: { background: "#e9d35b", color: "#fff" },
+        Title: "Full Stack Engineer",
+        Location: "ChapterX - Remote",
+        Icon: <WorkIcon />,
+        Period: "June 2022 - Present",
+        Description: [
+            <p>- Developed Web3 mini games by Unity 3D for metaverse product</p>,
+            <p>- Developed REST API in Node.js game server.</p>,
+            <p>- Developed admin panel by React.js for client data management.</p>,
+            <p>- Maintain Player Data in Postgres DB and AWS service.</p>,
+            <p>- Support solving Solana customer tech issues.</p>],
+    },
+    {
+        Format: { background: "#e9d35b", color: "#fff" },
+        Title: "VR Software Engineer",
+        Location: "HyperBot Studio - Remote",
+        Icon: <WorkIcon />,
+        Period: "Mar 2021 - Present",
+        Description: [
+            <p>- Developed VR fishing game "Galactic Catch", in charge of VR interaction, fishing mechanism, game logic and user interface.</p>,
+            <p>- Developed file I / O and storage system of VR music create tool "LyraVR".</p>]
+        ,
+    },
+    {
+        Format: { background: "#e9d35b", color: "#fff" },
+        Title: "Mobile Game Engineer",
+        Location: "Akatsuki Taiwan Inc. - Taipei, Taiwan",
+        Icon: <WorkIcon />,
+        Period: "Jan 2019 - June 2022",
+        Description: [
+            < p > - Developed Bandai game "ONE PIECE BON! BON! JOURNEY!!" , committed to client part development.</p>,
+            <p>- Maintain SQEX game "Romancing SaGa Re; univerSe", build service environment infrastructure on AWS.</p>,
+            <p>- Handle Product CI / CD on AWS and Jenkins</p>,
+            <p>- Lead the VR breakthrough team, research the latest technology and develop side projects</p>]
+        ,
+    },
+    {
+        Format: { background: "#e9d35b", color: "#fff" },
+        Title: "Streaming Game Engineer",
+        Location: "MegaLive Inc. - Taichung, Taiwan",
+        Icon: <WorkIcon />,
+        Period: "Apr 2017 - Jan 2019",
+        Description: [
+            <p>- Develop RTMP stream app and Nginx streaming server.</p>,
+            <p>- Develop Android / IOS game APP.</p>,
+            <p>- Implement CI system: Jenkins build game binary</p>]
+        ,
+    },
+    {
+        Format: { background: "#e9d35b", color: "#fff" },
+        Title: "Kiosk Game Engineer",
+        Location: "Bright Ideas Design Inc. - Taipei, Taiwan",
+        Icon: <WorkIcon />,
+        Period: "Oct 2016 - Apr 2017",
+        Description: [
+            <p>- Support interactive exhibition case of Taitung Museum of prehistory.</p>,
+            <p>- Developed interactive kiosk of 佛光山文化展示館.</p>,
+            <p>- In charge of 5 areas in 中科院軍方科技展示館 : AHQ simulator, UAV model, Reconnaissance Demo, Energy Introduction kiosks game.</p>]
+    },
+    // {
+    //     Format: { background: "#3e497a", color: "#fff" },
+    //     Title: "Kiosk Game Engineer",
+    //     Location: "Bright Ideas Design Inc. - Taipei, Taiwan",
+    //     Icon: <SchoolIcon />,
+    //     Period: "Oct 2016 - Apr 2017",
+    //     Description: [
+    //         <p>- Support interactive exhibition case of Taitung Museum of prehistory.</p>,
+    //         <p>- Developed interactive kiosk of 佛光山文化展示館.</p>,
+    //         <p>- In charge of 5 areas in 中科院軍方科技展示館 : AHQ simulator, UAV model, Reconnaissance Demo, Energy Introduction kiosks game.</p>]
+    // },
+];
--- src/Pages/Experience.js
+++ src/Pages/Experience.js
@@ -4,115 +4,31 @@ import {
     VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-import SchoolIcon from "@material-ui/icons/School";
-import WorkIcon from "@material-ui/icons/Work";
+import { ExperienceList } from "../Helpers/ExperienceList";
 
 function Experience() {
     return (
         <div className="experience">
-            <VerticalTimeline lineColor="#3e497a">
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--education"
-                    date="2014 - 2018"
-                    iconStyle={{ background: "#3e497a", color: "#fff" }}
-                    icon={<SchoolIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">
-                        My Cool University, Vancouver, British Columbia
-                    </h3>
+            <VerticalTimeline lineColor="#0fce2f">
+                {ExperienceList.map((exp, idx) => {
+                    return (
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--work"
+                            date={exp.Period}
+                            iconStyle={exp.Format}
+                            icon={exp.Icon}
+                        >
+                            <h3 className="vertical-timeline-element-title">
+                                {exp.Title}
+                            </h3>
+                            <h4 className="vertical-timeline-element-subtitle">
+                                {exp.Location}
+                            </h4>
+                            <p>{exp.Description}</p>
+                        </VerticalTimelineElement>
+                    );
+                })}
 
-                    <h4 className="vertical-timeline-element-subtitle">
-                        Bachelor's Degree
-                    </h4>
-
-                    <p> Computer Science</p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    date="2018 - 2020"
-                    iconStyle={{ background: "#e9d35b", color: "#fff" }}
-                    icon={<WorkIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">
-                        Back End Engineer - Google
-                    </h3>
-                    <h4 className="vertical-timeline-element-subtitle">
-                        San Francisco, CA
-                    </h4>
-                    <p>Developed the backend infrastructure for 3 projects.</p>
-                </VerticalTimelineElement>
-
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    date="2020 - present"
-                    iconStyle={{ background: "#e9d35b", color: "#fff" }}
-                    icon={<WorkIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">
-                        Full Stack Engineer - Twitch
-                    </h3>
-                    <h4 className="vertical-timeline-element-subtitle">
-                        San Francisco, CA
-                    </h4>
-                    <p>
-                        Helped the team launch 2 major features by working both in the front
-                        end and back end.
-                    </p>
-                </VerticalTimelineElement>
-
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    date="2020 - present"
-                    iconStyle={{ background: "#e9d35b", color: "#fff" }}
-                    icon={<WorkIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">
-                        Full Stack Engineer - Twitch
-                    </h3>
-                    <h4 className="vertical-timeline-element-subtitle">
-                        San Francisco, CA
-                    </h4>
-                    <p>
-                        Helped the team launch 2 major features by working both in the front
-                        end and back end.
-                    </p>
-                </VerticalTimelineElement>
-
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    date="2020 - present"
-                    iconStyle={{ background: "#e9d35b", color: "#fff" }}
-                    icon={<WorkIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">
-                        Full Stack Engineer - Twitch
-                    </h3>
-                    <h4 className="vertical-timeline-element-subtitle">
-                        San Francisco, CA
-                    </h4>
-                    <p>
-                        Helped the team launch 2 major features by working both in the front
-                        end and back end.
-                    </p>
-                </VerticalTimelineElement>
-
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    date="2020 - present"
-                    iconStyle={{ background: "#e9d35b", color: "#fff" }}
-                    icon={<WorkIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">
-                        Full Stack Engineer - Twitch
-                    </h3>
-                    <h4 className="vertical-timeline-element-subtitle">
-                        San Francisco, CA
-                    </h4>
-                    <p>
-                        Helped the team launch 2 major features by working both in the front
-                        end and back end.
-                    </p>
-                </VerticalTimelineElement>
             </VerticalTimeline>
         </div>
     );
