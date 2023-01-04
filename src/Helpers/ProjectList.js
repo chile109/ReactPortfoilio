29d6f5de33e7e603b91f97aa902c7bc3093b9d41
kevinshark <chile109@gmail.com>
2023-01-04 19:56:44 +0800

    update: projects data list

--- src/Helpers/ProjectList.js
+++ src/Helpers/ProjectList.js
@@ -1,17 +1,54 @@
 import project1 from "../Assets/Images/UCE-VR.png";
-import project2 from "../Assets/Images/UCE-VR.png";
 
 export const ProjectList = [
     {
         name: "UCE-VR",
         image: project1,
         video: "https://youtu.be/jjGUgkIYFpg",
-        skills: "Unity, Photon, XR interaction toolkit, Oculus Quest",
+        description: "A Unity Card Engine. Developer can easily create a multiplayer card game with Unity3D. It supports VR and AR.",
+        url: "https://www.cakeresume.com/portfolios/one-piece-bon-bon-journey"
     },
     {
-        name: "ClubDAO Admin Dashboard",
-        image: project2,
-        video: "https://youtu.be/jjGUgkIYFpg",
-        skills: "React,Node.js, Postgres, express, sequelize",
+        name: "Metaverse Gallery",
+        image: "https://media.cakeresume.com/image/upload/s--fqcn1aB7--/c_pad,fl_png8,h_621,w_828/v1672819190/dhjbmiktcmvuozrvjqxh.png",
+        video: "https://youtu.be/j9UkMfsJHyc",
+        description: "Webgl game developed by Unity3D. Player can visit digital museum and see NFT arts.Hot-Updated NFT arts and billboard with the admin panel.",
+        url: "https://www.cakeresume.com/portfolios/metaverse-gallery-admin-panel"
     },
+    {
+        name: "Web3 game - Avatar Arena",
+        image: "https://media.cakeresume.com/image/upload/s--l5tGLc8n--/c_pad,fl_png8,h_621,w_828/v1672817893/vpm17gzaagoee2xbxzzy.png",
+        video: "https://youtu.be/DYDg9kzUVKk",
+        description: "ChapterX’s first game experience — Avatar Arena. Users can use play as their NFT avatar in this epic PvP fighter game.",
+        url: "https://www.cakeresume.com/portfolios/web3-pvp-game-avatar-arena"
+    },
+    {
+        name: "Romancing SaGa Re;univerSe",
+        image: "https://images.cakeresume.com/post-images/f6b6d9ed-ad5e-41ec-a148-2d995ad2f188.jpeg",
+        video: "https://youtu.be/38NWz4aRfPM",
+        description: "SQUARE ENIX Mobile Game on AOS/IOS. Handle the backend development and maintenance.",
+        url: "https://www.cakeresume.com/portfolios/re-universe"
+    },
+    {
+        name: "ONE PIECE BON! BON! JOURNEY!!",
+        image: "https://media.cakeresume.com/image/upload/s--v_ukmVG_--/c_pad,fl_png8,h_621,w_828/v1645545890/sxnbtujtskc3wz97wlt0.png",
+        video: "https://youtu.be/6CbjxvXDqHw",
+        description: "Bandai Namco Mobile Game on AOS/IOS. Handle the Mobile development and maintenance.",
+        url: "https://www.cakeresume.com/portfolios/one-piece-bon-bon-journey"
+    },
+    {
+        name: "LyraVR",
+        image: "https://media.cakeresume.com/image/upload/s--1VrAb9Ar--/c_pad,fl_png8,h_621,w_828/v1646326179/ebkcei1sjemkcd3rzpei.png",
+        video: "https://www.youtube.com/watch?v=lJVgm34C3h0&ab_channel=LyraVR",
+        description: "Unity VR music create tool. Handle the NodeGraph file management system development.",
+        url: "https://www.cakeresume.com/portfolios/lyravr"
+    },
+    {
+        name: "CrazyGroceryVR",
+        image: "https://media.cakeresume.com/image/upload/s--70LqfmUw--/c_pad,fl_png8,h_621,w_828/v1533984165/pfsmdi26afeqtfz3zm3q.png",
+        video: "https://youtu.be/iH4V0dYmrwM",
+        description: "2018 XR Hack Fest Champion 『瘋狂砸貨店』. Simulate Taiwan traditional grocery store game that develop in Unity and SteamVR in 3 days",
+        url: "https://www.cakeresume.com/portfolios/vr-489794"
+    },
+
 ];
